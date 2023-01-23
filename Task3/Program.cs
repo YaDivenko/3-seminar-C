7373227c75130f1cafd1b043a36457204cac9627
Yana <yadivenko@gmail.com>
2023-01-23 14:10:52 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Таблица кубов чисел");
+
+for (int i = 1; i <= n; i++)
+{
+    System.Console.WriteLine($"{i} => {Math.Pow(i, 3)}");
+}
+
