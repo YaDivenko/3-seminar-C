cac3b6bf57ea388acd9e03ecdbf11fad5b18e0b0
Yana <yadivenko@gmail.com>
2023-01-23 14:08:04 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32 (Console.ReadLine ());
+int y1 = Convert.ToInt32 (Console.ReadLine ());
+int z1 = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Введите координаты второй точки");
+int x2 = Convert.ToInt32 (Console.ReadLine ());
+int y2 = Convert.ToInt32 (Console.ReadLine ());
+int z2 = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.WriteLine("Расстояние между двумя точками");
+System.Console.Write(Math.Round(Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) *1.0), 0.5),2));
