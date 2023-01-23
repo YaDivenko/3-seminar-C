84ccacf0f9b3dcaa1276fa9e750d20843d1f6a5d
Yana <yadivenko@gmail.com>
2023-01-23 14:06:06 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+string i = n.ToString();
+
+if (i[0]==i[4] & i[1]==i[3])
+{
+System.Console.WriteLine("Число является палиндромом");
+}
+
+else
+{
+    System.Console.WriteLine("Число не является палиндромом");
+};
+
+
+
